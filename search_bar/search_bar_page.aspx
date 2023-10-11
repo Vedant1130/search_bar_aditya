4537f55643c2a7c3e218e083851cc315ffe10f37
impos <impos@DESKTOP-77G6MH9>
2023-10-11 13:51:44 +0530

    Add project files.

---
+++ search_bar/search_bar_page.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="search_bar_page.aspx.cs" Inherits="search_bar.search_bar_page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Search kro veere</title>
+    <link rel="stylesheet" href="mystylr.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <nav>
+            <div>
+                <asp:TextBox placeholder="Search all Products" ID="TextBox1"
+                    CssClass="txtbx" runat="server" Height="25px" Width="350px" Rows="0" OnTextChanged="TextBox1_TextChanged">
+
+                </asp:TextBox>
+                <asp:Button ID="Button1" CssClass="bttn" runat="server"
+                    BackColor="#CCCC00" BorderColor="#CCCC00" BorderWidth="1px" Font-Names="Arial Rounded MT Bold"
+                    Height="25px" Text="Search" Width="105px" OnClick="Button1_Click" />
+            </div>
+        </nav>
+    </form>
+</body>
+</html>
---
+++ search_bar/search_bar_page.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+// CONSTRAINT [fk_ads_ToTable0] FOREIGN KEY ([acategoryid]) REFERENCES [dbo].[category] ([catid]),
+// CONSTRAINT[fk_ads_ToTable1] FOREIGN KEY([asubcatid]) REFERENCES[dbo].[subcategory]([subcatid])
+
+namespace search_bar
+{
+    public partial class search_bar_page : System.Web.UI.Page
+
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DB1.mdf;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+        }
+
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+
+
+
+        {
+            if (TextBox1.Text != "")
+            {
+                if ((TextBox1.Text == "tshirt") || (TextBox1.Text == "Tshirt"))
+                {
+                    Response.Redirect("Menus/Tshirt.aspx");
+                }
+                else if ((TextBox1.Text == "tshirt") || (TextBox1.Text == "Jeans"))
+                {
+                    Response.Redirect("Menus/Jeans.aspx");
+
+                }
+                else if ((TextBox1.Text == "shoes") || (TextBox1.Text == "Shoes"))
+                {
+                    Response.Redirect("Menus/Shoes.aspx");
+
+                }
+
+            }
+        }
+    }
+}
